20b4b94b6ab9d7c414d28ca3bbfd17d689970f49
NakChoi <nakchoi94@gmail.com>
2023-08-02 15:59:47 +0900

    # feat : Signle Response Dto 구현

---
+++ assignment/src/main/java/com/assignment/globalDto/SingleResponseDto.java
@@ -0,0 +1,14 @@
+package com.assignment.globalDto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class SingleResponseDto<T> {
+    private T data;
+}
+
+
+
