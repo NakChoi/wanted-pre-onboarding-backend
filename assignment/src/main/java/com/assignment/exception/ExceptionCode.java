8d22ecb5fef43cc2232ad70ebdf1fb94492cf17c
NakChoi <nakchoi94@gmail.com>
2023-08-02 23:57:17 +0900

    # feat : ExceptionCode 구현

--- assignment/src/main/java/com/assignment/exception/ExceptionCode.java
+++ assignment/src/main/java/com/assignment/exception/ExceptionCode.java
@@ -6,8 +6,12 @@ import org.springframework.http.HttpStatus;
 public enum ExceptionCode {
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "Member Not Found"),
     MEMBER_EXIST(HttpStatus.CONFLICT, "Member Exist"),
+    MEMBER_NOT_MATCH(HttpStatus.UNAUTHORIZED, "Member Not Matched"),
     PASSWORD_NOT_MATCH(HttpStatus.UNAUTHORIZED, "Password is Not Matched"),
-    ACCESSTOKEN_EXPIRATION(HttpStatus.UNAUTHORIZED, "AceessToken is Expired");
+    ACCESSTOKEN_EXPIRATION(HttpStatus.UNAUTHORIZED, "AceessToken is Expired"),
+    POST_EXISTS(HttpStatus.CONFLICT, "Post Exists"),
+    EMAIL_FORMAT_INCORRECT(HttpStatus.UNAUTHORIZED, "Email format Incorrect"),
+    PASSWORD_FORMAT_INCORRECT(HttpStatus.UNAUTHORIZED, "Password format Incorrect");
 
 
     @Getter
