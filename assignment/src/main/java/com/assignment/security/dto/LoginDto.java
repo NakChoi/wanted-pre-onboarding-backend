e0b28a5917a4d3e512f8b8322251fbc67a31e8bf
NakChoi <nakchoi94@gmail.com>
2023-08-05 17:52:24 +0900

    # refactor : Logindeto setter, 빌더 쓰기 위한 생성자 추가

--- assignment/src/main/java/com/assignment/security/dto/LoginDto.java
+++ assignment/src/main/java/com/assignment/security/dto/LoginDto.java
@@ -1,10 +1,16 @@
 package com.assignment.security.dto;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
 @Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginDto {
     private String username;
 
